b885c89fb05d70323a9ffdf8d4f34b576d718879
Eleanor Latus <eleanor@latus.co.uk>
2023-05-05 19:18:21 +0100

    track users current guess using keyup, check the pressed key is a letter using RegEx,  if a backspace then delete the most recent letter

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,12 +1,11 @@
 import {useState} from 'react'
-const [turn, setTurn] = useState(0) 
-const [currentGuess, setCurrentGuess] = useState('')
-const [guesses, setGuesses] = useState([]) // each guess is an array
-const [history, setHistory] = useState([]) // each guess is a string
-const [isCorrect, setIsCorrect] = useState(false)
 
 const useWordle = (solution) => {
-    
+    const [turn, setTurn] = useState(0) 
+    const [guesses, setGuesses] = useState([]) // each guess is an array
+    const [history, setHistory] = useState([]) // each guess is a string
+    const [isCorrect, setIsCorrect] = useState(false)
+    const [currentGuess, setCurrentGuess] = useState('')
 
     // format a guess into an array of letter objects
     const formatGuess = () => {
@@ -19,8 +18,21 @@ const useWordle = (solution) => {
     }
 
     //track typing and submit guess
-    const handleKeyUp = () => {
+    const handleKeyUp = ({ key }) => {
+        if(key === 'Backspace'){
+            setCurrentGuess((prev) => {
+                return prev.slice(0, -1)
+            })
+            return
+        }
 
+        if(/^[A-za-z]$/.test(key)){
+            if(currentGuess.length < 5){
+                setCurrentGuess((prev) => {
+                    return prev + key
+                })
+            }
+        }
     }
 
     return {turn, currentGuess, guesses, isCorrect, handleKeyUp}

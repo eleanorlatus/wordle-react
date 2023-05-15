2a7f43b90470c0a35dbddc9220d4adc21eb9838f
Eleanor Latus <eleanor@latus.co.uk>
2023-05-15 09:49:41 +0100

    Created grid and row components and added styling

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Row from './Row'
 
 export default function Grid({currentGuess, guesses, turn}) {
   return (
-    <div>Grid</div>
+    <div>
+        {guesses.map((guess, index) =>{
+            return <Row key={index}/>
+        })}
+    </div>
   )
 }

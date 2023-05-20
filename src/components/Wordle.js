a34dba60c07ae452929772b89e0b6eadef0158e2
Eleanor Latus <eleanor@latus.co.uk>
2023-05-20 13:01:14 +0100

    Added a box which shows when a user has either got the correct answer or has run out of guesses to end the game

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Modal from './Modal'
 
 export default function Wordle( { solution }) {
     const { currentGuess, handleKeyUp, guesses, isCorrect, turn, usedKeys} = useWordle(solution)
-
+    const [showModal, setShowModal] = useState(false)
     useEffect(()=> {
         window.addEventListener('keyup', handleKeyUp)
 
         if(isCorrect){
-          console.log("congrats, you win")
+          setTimeout(() => setShowModal(true), 2500)
           window.removeEventListener('keyup', handleKeyUp)
         }
 
         if(turn > 5){
-          console.log("Sorry, you ran out of turns")
+          setTimeout(() => setShowModal(true), 2500)
           window.removeEventListener('keyup', handleKeyUp)
         }
 
@@ -24,10 +25,10 @@ export default function Wordle( { solution }) {
 
   return (
     <>
-    <div>Solution- {solution}</div>
-    <div>Current guess = {currentGuess}</div>
+    <div>{solution}</div>
     <Grid currentGuess = {currentGuess} guesses={guesses} turn={turn}/>
     <Keypad usedKeys = {usedKeys}/>
+    {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution}/>}
     </>
   )
 }

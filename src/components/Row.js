c4a80e070f8705c95985d528721b77d1309bfe54
Eleanor Latus <eleanor@latus.co.uk>
2023-05-20 11:07:10 +0100

    Added animations to make the letters flip into the relevant colour

--- src/components/Row.js
+++ src/components/Row.js
@@ -19,7 +19,7 @@ export default function Row({guess, currentGuess}) {
                 {letters.map((letter, i) => (
                     <div key={i} className="filled">{letter}</div>
                 ))}
-                {[...Array(5-letters.length)].map((x,i) => (
+                {[...Array(5-letters.length)].map((_,i) => (
                     <div key={i}></div>
                 ))}
             </div>
